2f70fa5d0ded85f13943864699e91186cff866d3
Aleksandar Djindjic <aleksandar.djindjic@olmero.ch>
2018-08-02 15:16:52 +0200

    router example
    Signed-off-by: Aleksandar Djindjic <aleksandar.djindjic@olmero.ch>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 
-import { AppComponent } from './app.component';
+@Component({
+  selector: 'app-root',
+  template: `
+
+    <a routerLink="/page-one">one</a> |
+    <a routerLink="/page-two">two</a>
+    <div><router-outlet></router-outlet></div>
+  `,
+})
+export class AppComponent  {}
+
+@Component({template: 'page one'})
+export class PageOneComponent  {}
+
+@Component({template: 'page two'})
+export class PageTwoComponent  {}
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent, PageOneComponent, PageTwoComponent ],
   imports: [
+    RouterModule.forRoot([
+      { path: 'page-one', component: PageOneComponent },
+      { path: 'page-two', component: PageTwoComponent },
+      { path: '', component: PageTwoComponent },
+    ]),
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
